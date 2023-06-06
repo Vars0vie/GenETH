6c0c963c8bf4339a7db5875175d9895d30d52d59
Varsovie <130487200+Vars0vie@users.noreply.github.com>
2023-06-06 18:51:24 +0200

    Update index.js
    [+] Optimized

--- index.js
+++ index.js
@@ -1,97 +1,43 @@
 const Web3 = require('web3');
 const crypto = require('crypto');
-const fs = require("fs")
-const colors = require("couleurs")
+const fs = require('fs');
+const colors = require('couleurs');
 
-process.on('uncaughtException', function(err) {
-  
-});
+process.on('uncaughtException', function (err) {});
 
-function genandcheck() {
-  for (i = 0; i < 10; i++) {
-    let privateKey = crypto.randomBytes(32).toString('hex');
-    let random = Math.floor(Math.random() * (5 - 1 + 1) + 1)
-    if(random == 1){
-      let web3 = new Web3("https://eth.llamarpc.com");
-      let account = web3.eth.accounts.privateKeyToAccount(privateKey);
-      
-        web3.eth.getBalance(account.address).then((balance) => {
-          if (balance > 0) {
-            console.log(colors("[", [0, 149, 190]) + colors("+1", [0, 255, 0]) + colors("] ", [0, 149, 190]) + web3.utils.fromWei(balance, 'ether') + ':' + privateKey + ':' + account.address);
-            fs.appendFile('valid.txt', `${web3.utils.fromWei(balance, 'ether')}:${privateKey}:${account.address}` + '\n', function(err) {
-              if (err) throw err;
-            });
-          } else {
-            console.log(colors("[", [0, 149, 190]) + colors("-1", [255, 0, 0]) + colors("] ", [0, 149, 190]) + web3.utils.fromWei(balance, 'ether') + ':' + privateKey + ':' + account.address);
-          }
+const providers = [
+  'https://eth.llamarpc.com',
+  'https://rpc.builder0x69.io',
+  'https://ethereum.publicnode.com',
+  'https://rpc.mevblocker.io',
+  'https://ethereum.publicnode.com'
+];
+
+function getRandomProvider() {
+  const randomIndex = Math.floor(Math.random() * providers.length);
+  return providers[randomIndex];
+}
+
+function generateAndCheck() {
+  for (let i = 0; i < 10; i++) {
+    const privateKey = crypto.randomBytes(32).toString('hex');
+    const providerUrl = getRandomProvider();
+    const web3 = new Web3(providerUrl);
+    const account = web3.eth.accounts.privateKeyToAccount(privateKey);
+
+    web3.eth.getBalance(account.address).then((balance) => {
+      if (balance > 0) {
+        const output = `[+1] ${web3.utils.fromWei(balance, 'ether')}:${privateKey}:${account.address}`;
+        console.log(colors(output, [0, 149, 190]));
+        fs.appendFile('valid.txt', `${output}\n`, (err) => {
+          if (err) throw err;
         });
-
-    }
-    else if(random == 2){
-      let web3 = new Web3("https://rpc.builder0x69.io");
-      let account = web3.eth.accounts.privateKeyToAccount(privateKey);
-      
-        web3.eth.getBalance(account.address).then((balance) => {
-          if (balance > 0) {
-            console.log(colors("[", [0, 149, 190]) + colors("+1", [0, 255, 0]) + colors("] ", [0, 149, 190]) + web3.utils.fromWei(balance, 'ether') + ':' + privateKey + ':' + account.address);
-            fs.appendFile('valid.txt', `${web3.utils.fromWei(balance, 'ether')}:${privateKey}:${account.address}` + '\n', function(err) {
-              if (err) throw err;
-            });
-          } else {
-            console.log(colors("[", [0, 149, 190]) + colors("-1", [255, 0, 0]) + colors("] ", [0, 149, 190]) + web3.utils.fromWei(balance, 'ether') + ':' + privateKey + ':' + account.address);
-          }
-        });
-
-    }
-    else if(random == 3){
-      let web3 = new Web3("https://rpc.builder0x69.io");
-      let account = web3.eth.accounts.privateKeyToAccount(privateKey);
-      
-        web3.eth.getBalance(account.address).then((balance) => {
-          if (balance > 0) {
-            console.log(colors("[", [0, 149, 190]) + colors("+1", [0, 255, 0]) + colors("] ", [0, 149, 190]) + web3.utils.fromWei(balance, 'ether') + ':' + privateKey + ':' + account.address);
-            fs.appendFile('valid.txt', `${web3.utils.fromWei(balance, 'ether')}:${privateKey}:${account.address}` + '\n', function(err) {
-              if (err) throw err;
-            });
-          } else {
-            console.log(colors("[", [0, 149, 190]) + colors("-1", [255, 0, 0]) + colors("] ", [0, 149, 190]) + web3.utils.fromWei(balance, 'ether') + ':' + privateKey + ':' + account.address);
-          }
-        });
-
-    }
-    else if(random == 4){
-      let web3 = new Web3("https://ethereum.publicnode.com");
-      let account = web3.eth.accounts.privateKeyToAccount(privateKey);
-      
-        web3.eth.getBalance(account.address).then((balance) => {
-          if (balance > 0) {
-            console.log(colors("[", [0, 149, 190]) + colors("+1", [0, 255, 0]) + colors("] ", [0, 149, 190]) + web3.utils.fromWei(balance, 'ether') + ':' + privateKey + ':' + account.address);
-            fs.appendFile('valid.txt', `${web3.utils.fromWei(balance, 'ether')}:${privateKey}:${account.address}` + '\n', function(err) {
-              if (err) throw err;
-            });
-          } else {
-            console.log(colors("[", [0, 149, 190]) + colors("-1", [255, 0, 0]) + colors("] ", [0, 149, 190]) + web3.utils.fromWei(balance, 'ether') + ':' + privateKey + ':' + account.address);
-          }
-        });
-
-    }
-    else if(random == 5){
-      let web3 = new Web3("https://rpc.mevblocker.io");
-      let account = web3.eth.accounts.privateKeyToAccount(privateKey);
-      
-        web3.eth.getBalance(account.address).then((balance) => {
-          if (balance > 0) {
-            console.log(colors("[", [0, 149, 190]) + colors("+1", [0, 255, 0]) + colors("] ", [0, 149, 190]) + web3.utils.fromWei(balance, 'ether') + ':' + privateKey + ':' + account.address);
-            fs.appendFile('valid.txt', `${web3.utils.fromWei(balance, 'ether')}:${privateKey}:${account.address}` + '\n', function(err) {
-              if (err) throw err;
-            });
-          } else {
-            console.log(colors("[", [0, 149, 190]) + colors("-1", [255, 0, 0]) + colors("] ", [0, 149, 190]) + web3.utils.fromWei(balance, 'ether') + ':' + privateKey + ':' + account.address);
-          }
-        });
-
-    }
+      } else {
+        const output = `[-1] ${web3.utils.fromWei(balance, 'ether')}:${privateKey}:${account.address}`;
+        console.log(colors(output, [0, 149, 190]));
+      }
+    });
   }
 }
 
-setInterval(genandcheck, 50);
+setInterval(generateAndCheck, 50);
